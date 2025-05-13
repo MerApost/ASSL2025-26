db79d753883f3719255acd14af8c7a6a9af68485
Meropi Apostolaki <sdi2200008@uoa.gr>
2025-05-13 15:01:15 +0300

    Update musculoskeletal_system.py

--- Sub06/musculoskeletal_system.py
+++ Sub06/musculoskeletal_system.py
@@ -3,11 +3,11 @@ class MusculoskeletalSystem:
   def __init__(self, bones: int, skeletal_muscles: int):
     self.bones = bones
     self.skeletal_muscles = skeletal_muscles
-
+      
+  def check_move(self):
+    if (self.bones >= 206 and self.skeletal_muscles >= 600):
+      self.move = True
+    else:
+      self.move = False
     
-    def check_move(self):
-      if (self.bones >= 206 and self.skeletal_muscles >= 600):
-        self.move = True
-      else:
-        self.move = False
-      return self.move
+    return self.move
